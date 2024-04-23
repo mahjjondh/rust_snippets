d7f0b11fe57f01c6e26c24502b25afe376231844
mahjjongh <paulochicherchio@gmail.com>
2024-04-23 12:39:28 -0300

    Create conpost.rs

---
+++ conpost.rs
@@ -0,0 +1,26 @@
+use postgres::{error::Error, Client, NoTls};
+use std::time::SystemTime;
+#[derive(Debug)]
+struct NomedaStruct {
+    use std::time::SystemTime para tratar dados do tipo datetime
+}
+
+fn print_db() -> Result<(), Error>{
+    let mut conn = Client::connect("postgres://user:passsword@host:port/db" ,NoTls).unwrap();
+    for row in &conn.query("Query no banco, use CASE e WHEN para tratar colunas NULL ou que precisem ser tratadas",&[],)?
+    {
+        let var_do_tipo_struct = NomedaStruct{
+            nomecoluna: row.get(0),
+            
+        };
+        
+        println!("{}, {}", NomedaStruct.nomecoluna);
+    }
+Ok(())
+}
+
+
+fn main() -> Result<(), Error> {
+    print_db()?;
+    Ok(())
+}
